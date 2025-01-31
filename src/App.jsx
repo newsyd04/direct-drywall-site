98d2e33acf8b5b3ede92007893d30d54f3177d76
newsyd04 <newsyd04@gmail.com>
2025-01-31 13:52:12 +0000

    fix pages integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import Navbar from "./components/NavBar";
-import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage.jsx";
+import Navbar from "./components/NavBar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
