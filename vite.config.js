073e1924259839a48d30c664d8593fc040b1781f
newsyd04 <newsyd04@gmail.com>
2025-01-31 13:53:25 +0000

    fix pages integration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '//direct-drywall-site//',
+  base: '/direct-drywall-site/',
   plugins: [react()],
 })
